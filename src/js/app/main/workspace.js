1fa51a80c67473c1b256e5b713b13c227576d625
wangyongqiang-ds1 <996456665@qq.com>
2016-10-08 18:06:10 +0800

    解决冲突

--- src/js/app/main/workspace.js
+++ src/js/app/main/workspace.js
@@ -1,14 +1,15 @@
 /**
  * Created by wangyongqiang-ds1 on 2016/9/6.
  */
-define(['application','compShortcut','compSet','compText'],function (_app,compShortcut,compSet,compText) {
-    var workspace = {};
 
+define(['application','compShortcut','compSet','compText','compTemplate'],function (_app,compShortcut,compSet,compText,compTemplate) {
+    var workspace = {};
     workspace.init = function () {
 	    compShortcut.init();
         compSet.init();
         _app.init();
         compText.init();
+        compTemplate.init();
         console.log('workspace 我进来啦！');
     };
     return workspace;
