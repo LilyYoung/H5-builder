11dd06961b9152be3f862922c6ebbd9db07e788d
wangyongqiang-ds1 <996456665@qq.com>
2016-10-19 16:27:44 +0800

    增加可编辑操作工具

--- src/js/app/components/functionalAreas/comp-text.js
+++ src/js/app/components/functionalAreas/comp-text.js
@@ -13,21 +13,39 @@ define(function () {
                 $('.comp-drag').draggable('disable');
                 $(this).attr('contenteditable','true');
                 $(this).addClass('contentEdit');
-                if (window.getSelection) {
-                    var sel = window.getSelection();
-                    sel.modify('move','left','documentboundary');
-                    sel.modify('extend','right','documentboundary');
-                }
-                var isFireFox = function() {
-                    var ua = navigator.userAgent.toLowerCase();
-                    return !!ua.match(/firefox\/([\d.]+)/);
-                };
-                if (isFireFox()) {
-                    var count = htmlElement.innerHTML.split('').length;
-                    for (var i = 0; i < count; i++) {
-                        sel.modify('extend', 'right', 'line');
-                    }
-                }
+
+                // 创建一个textarea元素
+                var textAreaElement = document.createElement("textarea");
+                var divElement = $(this);
+                $('.editable-text').editable();
+                // // 把obj里面的元素以及文本内容赋值给新建的textAreaElement
+                // textAreaElement.value = divElement.innerHTML;
+                //
+                // // 用新建的textAreaElement代替原来的oldDivElement元素
+                // divElement.parentNode.replaceChild(textAreaElement, divElement);
+                // // 当textAreaElement失去焦点时触发下面函数，使得textarea变成div
+                // textAreaElement.onblur = function() {
+                //     //把input的值交给原来的div
+                //     divElement.innerHTML = textAreaElement.value;
+                //     //用原来的div重新替换textAreaElement
+                //     textAreaElement.parentNode.replaceChild(divElement, textAreaElement);
+                // };
+
+                // if (window.getSelection) {
+                //     var sel = window.getSelection();
+                //     sel.modify('move','left','documentboundary');
+                //     sel.modify('extend','right','documentboundary');
+                // }
+                // var isFireFox = function() {
+                //     var ua = navigator.userAgent.toLowerCase();
+                //     return !!ua.match(/firefox\/([\d.]+)/);
+                // };
+                // if (isFireFox()) {
+                //     var count = htmlElement.innerHTML.split('').length;
+                //     for (var i = 0; i < count; i++) {
+                //         sel.modify('extend', 'right', 'line');
+                //     }
+                // }
 
             });
             $(document).click(function () {
